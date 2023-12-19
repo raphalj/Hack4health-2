576c7b4bfba55c5521ba335c7abd70bce3114e05
Raphael Janssens <raphael.janssens@sciensano.be>
2023-12-19 16:40:35 +0100

    RJ Orange_figure setup

---
+++ Reporting-oranges.R
@@ -0,0 +1,48 @@
+###################################
+##### aim ####
+###################################
+#| produce quarto html docs in three language
+#| each doc needs two figures translated
+#| translation are located in csv file Hack4health-2\figure_translation.csv
+#| translate title, x, y axis, label legend
+#| render in the present script
+#| use github to collaborate on project each member developing a part of the code
+
+
+###################################
+##### package and data ####
+###################################
+library(dplyr)
+library(quarto)
+library(ggplot2)
+data("Orange")
+
+###################################
+##### Figure 1 ####
+###################################
+Orange %>%
+  ggplot(aes(x = circumference, y = age, colour = Tree)) +
+  geom_point()
+
+###################################
+##### figure 2 with sciensano theme ####
+###################################
+#| same figure with sciensano theme
+
+###################################
+##### figure 3 ####
+###################################
+#| same figure produced by a function save in fct.R
+#| fct with language in argument
+
+
+###################################
+##### render ####
+###################################
+# save image
+rm(list = ls()[grep(pattern = "df.raw", ls(), invert = F)])
+save.image("./wd.RData")
+
+# render quarto
+quarto_render("report.qmd", output_format = "html")
+# shell.exec(file.path("report.html", fsep = "\\"))

56b589edc7b3449276ae8e73187bdfd40033e7c9
LouiseVaes <118883304+LouiseVaes@users.noreply.github.com>
2023-12-20 12:07:24 +0100

    Creation lng_sel

--- fct.R
+++ fct.R
@@ -2,21 +2,37 @@
 ##### function ####
 ###################################
 
+#### Language selection ####
 
+Lng_sel <- function(Type, Var, Lng){
+  Lng_sel <- Table_lng %>%
+    filter(type == Type, str_detect(var, Var)) %>%
+    select(Lng) %>%
+    as.vector %>%
+    unlist
+  return(Lng_sel)
+}
 
 #### Table ####
 Orange_table_kbl <- function(Data, Table_lng, Lng){
   
-  colnames <- Table_lng %>%
-    filter(type == "Orange_table", var == "colnames") 
+  colnames <- Lng_sel("Orange_table", "col", Lng)
   
-  caption <- Table_lng %>%
-    filter(type == "Orange_table", var == "caption") 
+  # colnames <- Table_lng %>%
+  #   filter(type == "Orange_table", str_detect(var, "col")) %>%
+  #   select(Lng) %>%
+  #   as.vector() %>%
+  #   unlist()
   
-  footnote <- Table_lng %>%
-    filter(type == "Orange_table", var == "footnote") 
+  caption <- Lng_sel("Orange_table", "caption", Lng)
+
+  # caption <- Table_lng %>%
+  #   filter(type == "Orange_table", var == "caption") %>%
+  #   select(Lng) %>%
+  #   as.vector() %>%
+  #   unlist()
   
- Data_min <- Data %>%
+  Data_min <- Data %>%
     group_by(Tree) %>%
     summarise_at(vars(c(age, circumference)), list(min = min))
   
@@ -27,10 +43,10 @@ Orange_table_kbl <- function(Data, Table_lng, Lng){
   Data_table <- merge(Data_min, Data_max, by = "Tree")
   
   kable(Data_table, 
-        col.names = c("Tree", "min age", "min circumference","max age", "max circumference"),
+        # col.names = c("Tree", "min age", "min circumference","max age", "max circumference"),
+        col.names = colnames,
         align = "c", 
         # caption = "Min and max age and circumference by tree group", 
-        caption = caption[[Lng]],
-        footnote = footnote[[Lng]])
+        caption = caption)
   
 }
